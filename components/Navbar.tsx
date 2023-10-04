94897d0ca2bc2293fb13e4249bd4dde04062544f
MAGESHKANNAN ANNATHURAI <mageshkannanam@gmail.com>
2023-10-04 18:49:31 +0530

    added sidebar along with  navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,28 @@
 import Link from "next/link";
 import { ThemeToggle } from "./ThemeToggle";
+import Sidebar from "./Sidebar";
+import { Button } from "./ui/button";
 function Navbar() {
   return (
     <nav className="fixed top-0 z-20 flex w-screen items-center  justify-between bg-white p-3 px-3 shadow-lg dark:bg-black">
-      <Link href="/" className="text-2xl font-semibold">
-        Next-Store
-      </Link>
-      <ThemeToggle />
+      <span className="flex items-center gap-10">
+        <Link href="/" className="text-2xl font-bold">
+          Next-Store🛍️
+        </Link>
+        <Button
+          asChild
+          variant={"ghost"}
+          className=" hidden border-2 border-primary sm:block"
+        >
+          <Link href="/products" className="text-xl">
+            Products
+          </Link>
+        </Button>
+      </span>
+      <span className="flex gap-3">
+        <ThemeToggle />
+        <Sidebar />
+      </span>
     </nav>
   );
 }
---
+++ components/Sidebar.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTrigger,
+} from "./ui/sheet";
+import { GanttChart } from "lucide-react";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+
+function Sidebar() {
+  return (
+    <Sheet>
+      <SheetTrigger className="sm:hidden">
+        <GanttChart />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader className="text-2xl font-semibold">
+          NextStore🛍️
+        </SheetHeader>
+        <Link
+          href="/products"
+          className="mt-3 text-xl font-semibold underline underline-offset-4"
+        >
+          View Products
+        </Link>
+        <h3 className="py-3 text-xl font-semibold">Categories</h3>
+        <Separator />
+        <div className="flex flex-col gap-2">
+          <Button variant="ghost" asChild>
+            <Link href="/products">Men</Link>
+          </Button>
+          <Button variant="ghost" asChild>
+            <Link href="/products">Women</Link>
+          </Button>
+          <Button variant="ghost" asChild>
+            <Link href="/products">Electronics</Link>
+          </Button>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default Sidebar;
