c33793ca37a426304f9c05f6f06298ee3616284a
MAGESHKANNAN ANNATHURAI <mageshkannanam@gmail.com>
2023-10-13 19:32:44 +0530

    added og image

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,53 @@
+export const metadata: Metadata = {
+  title: "Next Store | Products",
+  description: "An E-commerce website built with Next.js, Stripe",
+  keywords: [
+    "Next.js",
+    "React",
+    "JavaScript",
+    "Stripe",
+    "E-commerce",
+    "E-commerce website",
+    "Next.js E-commerce website",
+    "Stripe checkout",
+  ],
+  authors: [
+    {
+      name: "Mageshkannan",
+      url: "https://mageshkannan.netlify.app",
+    },
+  ],
+  creator: "Mageshkannan",
+  openGraph: {
+    title: "Next Store",
+    description: "An E-commerce website built with Next.js, Stripe",
+    images: [
+      {
+        url: "https://nextstore-stripe.vercel.app/next-store-og.png",
+        width: 1200,
+        height: 600,
+        alt: "next store",
+      },
+    ],
+  },
+  twitter: {
+    title: "Next Store",
+    description: "An E-commerce website built with Next.js, Stripe",
+    images: [
+      {
+        url: "https://nextstore-stripe.vercel.app/next-store-og.png",
+        width: 1200,
+        height: 600,
+        alt: "next store",
+      },
+    ],
+  },
+  generator: "Next.js",
+  applicationName: "Next Store",
+  publisher: "Mageshkannan",
+  colorScheme: "dark",
+};
+
 export default function Home() {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center">
