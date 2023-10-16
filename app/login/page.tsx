28c3f3b8e73b73fd70d8f889422d0b36f993f177
MAGESHKANNAN ANNATHURAI <mageshkannanam@gmail.com>
2023-10-16 09:38:51 +0530

    added login form and pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,14 @@
+import LoginForm from "@/components/LoginForm";
+import Link from "next/link";
+
 export default function Login() {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center">
       <h1>Login Page</h1>
+      <LoginForm />
+      <p>
+        new user? <Link href="/signup">Signup</Link>
+      </p>
     </main>
   );
 }
