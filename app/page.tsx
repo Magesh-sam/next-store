6b686b2122f7085e2f11bdba5e7e763e76b7cc33
MAGESHKANNAN ANNATHURAI <mageshkannanam@gmail.com>
2023-10-17 11:59:46 +0530

    formatted code

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,6 @@ export default function Home() {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center">
       <h1>Welcome to next store built with next js</h1>
-      
     </main>
   );
 }
