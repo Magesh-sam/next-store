eb21cc3e382c5a32087ac31caf667ce981123487
mageshkannan <mageshkannanam@gmail.com>
2023-10-23 19:27:05 +0530

    added quantity button

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -4,12 +4,11 @@ import { db } from "@/firebase/config";
 import { getSession } from "@auth0/nextjs-auth0";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import Image from "next/image";
-import { redirect } from "next/navigation";
 
 export default async function Cart() {
   const session = await getSession();
   const user = session?.user;
-  const citiesRef = collection(db, "cartitems");
+  const cartitemsRef = collection(db, "cartitems");
   if (!user) {
     return (
       <>
@@ -18,21 +17,35 @@ export default async function Cart() {
     );
   }
 
-  const q = query(citiesRef, where("uid", "==", user?.email));
+  const q = query(cartitemsRef, where("uid", "==", user?.email));
   const querySnapshot = await getDocs(q);
+  console.log(
+    "Snapshot",
+    querySnapshot.docs.map((doc) => doc.id),
+  );
   const data = querySnapshot.docs.map((doc) => doc.data());
-
+  const dataId = querySnapshot.docs.map((doc) => doc.id);
+  const finalItems = data.map((item, index) => {
+    return {
+      id: dataId[index],
+      image: item.image,
+      title: item.title,
+      price: item.price,
+      quantity: item.quantity,
+    };
+  });
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center">
       <h1>Cart</h1>
 
-      {data.map((item) => (
+      {finalItems.map((item) => (
         <div key={item.id} className="flex">
           <Image src={item.image} alt={item.title} width={200} height={200} />
           <div className="flex flex-col">
             <h3>{item.title}</h3>
             <p>${item.price}</p>
-            <QuantityBtn quantity={item.quantity} />
+            <p>product id:{item.id}</p>
+            <QuantityBtn quantity={item.quantity} productId={item.id} />
           </div>
         </div>
       ))}
