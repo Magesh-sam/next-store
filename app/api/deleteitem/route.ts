3105b4b669ec8aa069555347fa188b7c0b2aad04
mageshkannan <mageshkannanam@gmail.com>
2023-10-26 09:58:24 +0530

    added api routes for cart/wishlist

---
+++ app/api/deleteitem/route.ts
@@ -0,0 +1,20 @@
+import { db } from "@/firebase/config";
+import { deleteDoc, doc } from "firebase/firestore";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function POST(req: NextRequest) {
+  const { id }: { id: string } = await req.json();
+  try {
+    await deleteDoc(doc(db, "cartitems", id));
+    return NextResponse.json({
+      success: true,
+      status: 200,
+    });
+  } catch (err) {
+    return NextResponse.json({
+      success: false,
+      status: 500,
+      error: err,
+    });
+  }
+}
