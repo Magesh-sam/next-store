7697e2b8889573045dc31591961528e9fdacc651
mageshkannan <mageshkannanam@gmail.com>
2023-10-26 18:44:24 +0530

    fixed prev prod build

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,11 +1,6 @@
-import axios from "axios";
 
 export default async function Wishlist() {
-  const data = axios
-    .post("/api/wishlist", {
-      "id": "mageshkannanam@gmil.com",
-    })
-    .then((res) => res.data);
+ 
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center">
       <h1 className="text-5xl">Wishlist</h1>
